66ed5814f76d0d032fa56dc42f8799ade4326295
Oleksandr Polishchuk <60441894+Atlass-cyber@users.noreply.github.com>
2025-10-07 22:05:26 +0300

    Move IUdpClient.cs into NetSdrClient.Networking namespace

--- NetSdrClientApp/Networking/IUdpClient.cs
+++ NetSdrClientApp/Networking/IUdpClient.cs
@@ -1,10 +1,12 @@
-﻿
-public interface IUdpClient
+﻿namespace NetSdrClient.Networking 
 {
-    event EventHandler<byte[]>? MessageReceived;
-
-    Task StartListeningAsync();
-
-    void StopListening();
-    void Exit();
-}
+    public interface IUdpClient
+    {
+        event EventHandler<byte[]>? MessageReceived;
+    
+        Task StartListeningAsync();
+    
+        void StopListening();
+        void Exit();
+    }
+ }
