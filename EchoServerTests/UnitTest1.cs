a4302da090d8a0de9734754bcfc0b722c5d7da9b
Oleksandr Polishchuk <60441894+Atlass-cyber@users.noreply.github.com>
2025-11-05 08:42:10 +0000

    fix: Refactor EchoServer and add unit tests

---
+++ EchoServerTests/UnitTest1.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+using NetSdrClientApp.Server; 
+
+namespace EchoServerTests
+{
+    public class EchoLogicTests
+    {
+        [Test] 
+        public void ProcessMessage_ShouldReturnSameBytes_WhenCalled()
+        {
+            var logic = new EchoLogic();      
+            byte[] inputMessage = new byte[] { 10, 20, 30 };
+            byte[] expectedResponse = new byte[] { 10, 20, 30 };
+            byte[] actualResponse = logic.ProcessMessage(inputMessage, inputMessage.Length);
+
+            CollectionAssert.AreEqual(expectedResponse, actualResponse);
+        }
+
+        [Test]
+        public void ProcessMessage_ShouldHandlePartialBuffer_WhenCalledCorrectly()
+        {
+            // Arrange
+            var logic = new EchoLogic();
+            byte[] largeInputBuffer = new byte[] { 1, 2, 3, 0, 0, 0, 0 }; 
+            int bytesToProcess = 3; 
+            byte[] expectedResponse = new byte[] { 1, 2, 3 };
+
+            // Act
+            byte[] actualResponse = logic.ProcessMessage(largeInputBuffer, bytesToProcess);
+
+            // Assert
+            CollectionAssert.AreEqual(expectedResponse, actualResponse, "Логіка мала 'обрізати' буфер до реального розміру");
+        }
+    }
+}
