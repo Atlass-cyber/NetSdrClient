3ffdf8f61afe755f996d6b29aab121edbe7a97f9
Oleksandr Polishchuk <60441894+Atlass-cyber@users.noreply.github.com>
2025-11-17 11:23:51 +0200

    fix: code smells x2 after PR NetSdrMessageHelperTests.cs

--- NetSdrClientAppTests/NetSdrMessageHelperTests.cs
+++ NetSdrClientAppTests/NetSdrMessageHelperTests.cs
@@ -35,12 +35,12 @@ namespace NetSdrClientAppTests
             //Assert
             Assert.Multiple(() =>
             {
-                Assert.That(headerBytes.Count(), Is.EqualTo(2));
-                Assert.That(msg.Length, Is.EqualTo(actualLength));
+                Assert.That(headerBytes.Count(), Is.EqualTo(2)); 
+                Assert.That(msg, Has.Length.EqualTo(actualLength));
                 Assert.That(type, Is.EqualTo(actualType));
                 Assert.That(actualCode, Is.EqualTo((short)code));
                 
-                Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
+                 Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
             });
         }
 
@@ -64,11 +64,11 @@ namespace NetSdrClientAppTests
             //Assert
             Assert.Multiple(() =>
             {
-                Assert.That(headerBytes.Count(), Is.EqualTo(2));
-                Assert.That(msg.Length, Is.EqualTo(actualLength));
+                Assert.That(headerBytes.Count(), Is.EqualTo(2)); 
+                Assert.That(msg, Has.Length.EqualTo(actualLength)); 
                 Assert.That(type, Is.EqualTo(actualType));
                 
-                Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
+  s.Count(), Is.EqualTo(parametersLength)); 
             });
 
     }
