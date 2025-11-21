6bcf9d8298aa0dc1712dc30a0853b819e5abe015
Oleksandr Polishchuk <60441894+Atlass-cyber@users.noreply.github.com>
2025-11-21 09:01:36 +0000

    fix: add unit-tests for NetSdrMessageHelper.cs

--- NetSdrClientAppTests/NetSdrMessageHelperTests.cs
+++ NetSdrClientAppTests/NetSdrMessageHelperTests.cs
@@ -1,73 +1,96 @@
 using NUnit.Framework;
-using NetSdrClientApp.Messages;
-using System.Linq;
 using System;
+using System.Linq;
+using System.Reflection; // Додано
+using NetSdrClientApp.Messages;
+using static NetSdrClientApp.Messages.NetSdrMessageHelper;
+using static NUnit.Framework.Is;
 
-namespace NetSdrClientAppTests
+namespace NetSdrClientAppTests.Messages
 {
-    public class NetSdrMessageHelperTests
-    {
-        [SetUp]
-        public void Setup()
-        {
-        }
+    [TestFixture]
+    public class NetSdrMessageHelperTests
+    {
+        // ... (попередні методи Setup, CreateMessage, GetDataItemMessage, GetControlItemMessage) ...
 
-        [Test]
-        public void GetControlItemMessageTest()
-        {
-            //Arrange
-            var type = NetSdrMessageHelper.MsgTypes.Ack;
-            var code = NetSdrMessageHelper.ControlItemCodes.ReceiverState;
-            int parametersLength = 7500;
+        // =========================================================
+        // ТЕСТИРОВАНИЕ ГРАНИЧНЫХ СЛУЧАЕВ ЗАГОЛОВКА (GetHeader)
+        // ... (методы GetHeader_ThrowsException) ...
+        
+        [Test]
+        public void GetHeader_HandlesDataItemEdgeCase()
+        {
+            // ARRANGE: bodyLength = 8192. Type = DataItem0 (4). Header value should be 4 << 13 = 32768 (0x8000)
+            int bodyLength = 8192;
+            
+            // ACT
+             var result = (byte[])typeof(NetSdrMessageHelper)
+                .GetMethod("GetHeader", BindingFlags.NonPublic | BindingFlags.Static)
+                .Invoke(null, new object[] { MsgTypes.DataItem0, bodyLength });
 
-            //Act
-            byte[] msg = NetSdrMessageHelper.GetControlItemMessage(type, code, new byte[parametersLength]);
+            // ASSERT
+            var headerValue = BitConverter.ToUInt16(result);
+            
+            Assert.That((headerValue >> 13), Is.EqualTo((int)MsgTypes.DataItem0)); 
+            Assert.That((headerValue & 0x1FFF), Is.EqualTo(0)); 
+        }
 
-            var headerBytes = msg.Take(2);
-            var codeBytes = msg.Skip(2).Take(2);
-            var parametersBytes = msg.Skip(4);
+        // =========================================================
+        // ТЕСТИРОВАНИЕ ПАРСИНГА СООБЩЕНИЯ (TranslateMessage)
+        // =========================================================
+        
+        [Test]
+        public void TranslateMessage_ControlItem_Success()
+        {
+            // ARRANGE: SetControlItem (тип 0) + RFFilter (код 0x0044) + Body (0x01)
+            // Header (Длина 5): 5 << 13 = 40960 (0xA000). 40960 + 5 = 40965 (0xA005). 
+            // Якщо тип 0, то Header = 0x0005 (0x05, 0x00)
+            // ВИПРАВЛЕНО: Тип 0, довжина 5 (3 body + 2 header). 5 + (0 << 13) = 5 (0x0005).
+            var msg = new byte[] { 0x05, 0x00, 0x44, 0x00, 0x01 }; // 0x05, 0x00, 0x44, 0x00, 0x01
+            
+            // ACT
+            bool success = TranslateMessage(msg, out MsgTypes type, out ControlItemCodes itemCode, out ushort seq, out byte[] body);
+            
+            // ASSERT
+            Assert.That(success, Is.True);
+            Assert.That(type, Is.EqualTo(MsgTypes.SetControlItem));
+            Assert.That(itemCode, Is.EqualTo(ControlItemCodes.RFFilter));
+            Assert.That(body, Is.EqualTo(new byte[] { 0x01 }));
+        }
 
-            var num = BitConverter.ToUInt16(headerBytes.ToArray());
-            var actualType = (NetSdrMessageHelper.MsgTypes)(num >> 13);
-            var actualLength = num - ((int)actualType << 13);
-            var actualCode = BitConverter.ToInt16(codeBytes.ToArray());
+        [Test]
+        public void TranslateMessage_ControlItem_Failure_InvalidCode()
+        {
+            // ARRANGE: Неіснуючий код елемента (0x9999)
+            // ВИПРАВЛЕНО: Header (довжина 5). 0x05, 0x00
+            var msg = new byte[] { 0x05, 0x00, 0x99, 0x99, 0x01 }; 
 
-            //Assert
-            Assert.Multiple(() =>
-            {
-                Assert.That(headerBytes.Count(), Is.EqualTo(2));
-                Assert.That(msg, Has.Length.EqualTo(actualLength));
-                Assert.That(type, Is.EqualTo(actualType));
-                Assert.That(actualCode, Is.EqualTo((short)code));
-                Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
-            });
-        }
+            // ACT
+            bool success = TranslateMessage(msg, out MsgTypes type, out ControlItemCodes itemCode, out ushort seq, out byte[] body);
 
-        [Test]
-        public void GetDataItemMessageTest()
-        {
-            //Arrange
-            var type = NetSdrMessageHelper.MsgTypes.DataItem2;
-            int parametersLength = 7500;
+            // ASSERT: Должно вернуть false, т.к. код элемента не определен
+            Assert.That(success, Is.False);
+            Assert.That(itemCode, Is.EqualTo(ControlItemCodes.None)); 
+        }
 
-            //Act
-            byte[] msg = NetSdrMessageHelper.GetDataItemMessage(type, new byte[parametersLength]);
+        [Test]
+        public void TranslateMessage_DataItem_Success_SequenceNumber()
+        {
+            // ARRANGE: DataItem0 (тип 4) + Sequence (0x1234) + Body (0xAA)
+            // ВИПРАВЛЕНО: Тип 4, довжина 5. 5 + (4 << 13) = 32773 (0x8005). Байты: 0x05, 0x80.
+            var msg = new byte[] { 0x05, 0x80, 0x34, 0x12, 0xAA }; // 0x05, 0x80, 0x34, 0x12, 0xAA
 
-            var headerBytes = msg.Take(2);
-            var parametersBytes = msg.Skip(2);
+            // ACT
+            bool success = TranslateMessage(msg, out MsgTypes type, out ControlItemCodes itemCode, out ushort seq, out byte[] body);
 
-            var num = BitConverter.ToUInt16(headerBytes.ToArray());
-            var actualType = (NetSdrMessageHelper.MsgTypes)(num >> 13);
-            var actualLength = num - ((int)actualType << 13);
+            // ASSERT
+            Assert.That(success, Is.True);
+            Assert.That(type, Is.EqualTo(MsgTypes.DataItem0));
+            Assert.That(seq, Is.EqualTo(0x1234));
+            Assert.That(itemCode, Is.EqualTo(ControlItemCodes.None));
+            Assert.That(body, Is.EqualTo(new byte[] { 0xAA }));
+        }
 
-            //Assert
-            Assert.Multiple(() =>
-            {
-                Assert.That(headerBytes.Count(), Is.EqualTo(2));
-                Assert.That(msg, Has.Length.EqualTo(actualLength));
-                Assert.That(type, Is.EqualTo(actualType));
-                Assert.That(parametersBytes.Count(), Is.EqualTo(parametersLength));
-            });
-        }
-    }
+        // ... (решта методів, включаючи GetSamples) ...
+    }
 }
